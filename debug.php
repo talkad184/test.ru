d05b91f9110658fe13877c448c9dbc5ff89ee732
talkad184 <82645044+talkad184@users.noreply.github.com>
2021-04-22 00:31:44 +0300

    Update debug.php

--- debug.php
+++ debug.php
@@ -2,37 +2,49 @@
 
 require_once 'vendor/autoload.php';
 require_once 'vendor/mashape/unirest-php/src/Unirest.php';
-require_once 'config.php';
+require 'config.php';
 
-$syncData = [];
-$headers = array(
+$ch = curl_init();
+$syncDatat = [];
+$syncDatay = [];
+$headerst = array(
   'Accept' => 'application/json'
 );
 
-$response = Unirest\Request::get(
+$responset = Unirest\Request::get(
   'https://api.trello.com/1/boards/eFOyz7pc/cards',
-  $headers,
-  $query
+  $headerst,
+  $queryt
 );
 
-$responseData = $response->raw_body;
-$responseAsArray = json_decode($responseData, true);
+$responsetData = $responset->raw_body;
+$responsetAsArray = json_decode($responsetData, true);
 
-foreach($responseAsArray as $data) {
-    $description = $data["desc"];
-    $name = $data["name"];
-    $members = $data["idMembers"];
-    $labels = $data["labels"];
-    $syncData[] = [
-        'name' => $name,
-        'description' => $description,
-        'members' => $members,
-        'labels' => $labels
+foreach($responsetAsArray as $datat) {
+    $descriptiont = $datat["desc"];
+    $namet = $datat["name"];
+    $syncDatat[] = [
+        'summary' => $namet,
+        'description' => $descriptiont
         ];
 }
 
 
-var_dump($syncData);
-die();
+$responsey = Unirest\Request::get(
+  'https://talkad184.myjetbrains.com/youtrack/api/issues?fields=summary,description',
+  $headersy
+);
+
+$responseyData = $responsey->raw_body;
+$responseyAsArray = json_decode($responseyData, true);
+
+foreach($responseyAsArray as $datay) {
+    $descriptiony = $datay['description'];
+    $namey = $datay['summary'];
+    $syncDatay[] = [
+        'summary' => $namey,
+        'description' => $descriptiony
+        ];
+};
 
 ?>
