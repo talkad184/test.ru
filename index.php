8ed9c567e57453e4350a896931622a7332b3adb9
talkad184 <82645044+talkad184@users.noreply.github.com>
2021-04-22 00:31:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,21 +2,37 @@
 
 require_once 'vendor/autoload.php';
 require_once 'vendor/mashape/unirest-php/src/Unirest.php';
-require_once 'config.php';
+require 'config.php';
+require_once 'debug.php';
 
-$headers = array(
-  'Accept' => 'application/json'
-);
+print_r($syncDatat);
+print_r($syncDatay);
 
-$response = Unirest\Request::get(
-  'https://api.trello.com/1/boards/eFOyz7pc/cards',
-  $headers,
-  $query
-);
+/*$ch = curl_init();
 
-$responseData = $response->raw_body;
-$responseAsArray = json_decode($responseData, true);
+$post = array (
+    'project' => array ('id' => "0-1"),
+    'summary' => 'test,test',
+    'description' => 'testeststs testeststs testeststs testeststs testeststs');
 
-print_r($responseData);
+$post = json_encode($post);
+curl_setopt($ch, CURLOPT_URL, 'https://talkad184.myjetbrains.com/youtrack/api/issues');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+$headers = array();
+$headers[] = 'Accept: application/json';
+$headers[] = 'Authorization: Bearer perm:cm9vdA==.NTEtMA==.tRN0kr0usrlL0ChAftvUAArR7ESq0B';
+$headers[] = 'Content-Type: application/json';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+var_dump($result);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+*/
 
 ?>
