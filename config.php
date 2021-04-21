322e422c3f8d29160cc12ca5ff5dc7b42a815b7c
talkad184 <82645044+talkad184@users.noreply.github.com>
2021-04-22 00:31:41 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
 
-$query = array(
+$queryt = array(
   'key' => 'c9d9aecc67c0c4cb32f956ee53667d9b',
   'token' => 'f1d2ce8815509345e78c827988752788ae4536997ea579024de10e18bb4ed2b7'
+);
+
+$headersy = array(
+  'Accept' => 'application/json',
+  'Authorization' => 'Bearer perm:cm9vdA==.NTEtMA==.tRN0kr0usrlL0ChAftvUAArR7ESq0B',
+  'Content-Type' => 'application/json'
 );
 
 ?>
