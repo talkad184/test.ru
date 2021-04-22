2ee91040d722b6a86cfb657194848e301aff1404
talkad184 <82645044+talkad184@users.noreply.github.com>
2021-04-22 13:12:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,34 +5,53 @@ require_once 'vendor/mashape/unirest-php/src/Unirest.php';
 require 'config.php';
 require_once 'debug.php';
 
+$statusy = array();
+$statust = array();
+$ch = curl_init();
+
 print_r($syncDatat);
 print_r($syncDatay);
 
-/*$ch = curl_init();
-
-$post = array (
-    'project' => array ('id' => "0-1"),
-    'summary' => 'test,test',
-    'description' => 'testeststs testeststs testeststs testeststs testeststs');
-
-$post = json_encode($post);
-curl_setopt($ch, CURLOPT_URL, 'https://talkad184.myjetbrains.com/youtrack/api/issues');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-
-$headers = array();
-$headers[] = 'Accept: application/json';
-$headers[] = 'Authorization: Bearer perm:cm9vdA==.NTEtMA==.tRN0kr0usrlL0ChAftvUAArR7ESq0B';
-$headers[] = 'Content-Type: application/json';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-$result = curl_exec($ch);
-var_dump($result);
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
+foreach($syncDatay as $valuey){
+    $statusy[] = $valuey['summary'];
 }
-curl_close($ch);
-*/
+
+print_r($statusy);
+
+foreach($syncDatat as $valuet){
+    $statust[] = $valuet['summary'];
+}
+
+print_r($statust);
+
+$diffsummary = array_diff($statust, $statusy);
+print_r($diffsummary);
+$dst = count($diffsummary);
+
+foreach ($diffsummary as $post){
+    $post = array (
+        'project' => array ('id' => "0-1"),
+        'summary' => " $diffsummary[$dst]",
+        'description' => "from trello");
+
+    $post = json_encode($post);
+    curl_setopt($ch, CURLOPT_URL, 'https://talkad184.myjetbrains.com/youtrack/api/issues');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+    $headers = array();
+    $headers[] = 'Accept: application/json';
+    $headers[] = 'Authorization: Bearer perm:cm9vdA==.NTEtMA==.tRN0kr0usrlL0ChAftvUAArR7ESq0B';
+    $headers[] = 'Content-Type: application/json';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $result = curl_exec($ch);
+    var_dump($result);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }
+    curl_close($ch);
+}
 
 ?>
